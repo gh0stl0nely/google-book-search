f5defbca2178484e93de867ef1938b2b48d655fc
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-06-20 12:30:48 -0400

    Deployed

--- server.js
+++ server.js
@@ -24,9 +24,7 @@ if(process.env.NODE_ENV === 'production'){
     app.get("*", (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-} else {
-    require("dotenv").config();
-}
+};
 
 // Connect to PORT 
 app.listen(PORT, function(){
